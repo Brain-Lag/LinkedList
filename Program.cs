ee53eb4bfe5d9e02122b706f065dc170e1d881c3
levchenkolk <levchenkolk@313D-10>
2023-02-09 16:05:56 +0700

    1

--- Program.cs
+++ Program.cs
@@ -2,26 +2,33 @@
 
 while (true)
 {
-    Console.WriteLine("Введите длину массива");
-    if (!int.TryParse(Console.ReadLine(), out int result)) result = 0;
-    int[] arr = new int[result];
-    while (true)
+    Console.WriteLine("Введите элементы массива через Enter. ()");
+
+    int[] arr = new int[0];
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int result)) ArrayAdd(ref arr, result);
+        else break;
+    }
+    ArrayPrint(arr);
+
+    bool check = true;
+    while (check)
     {
         int choice;
-        Console.WriteLine("Выберите действие: 1)Add \n2)Remove \n3)Print");
+        Console.WriteLine("Выберите действие:\n1)Add \n2)Remove \n3)Print");
         choice = int.Parse(Console.ReadLine());
         switch (choice)
         {
             case 1:
                 Console.WriteLine("Введите добавляемое число");
-                if (!int.TryParse(Console.ReadLine(), out result))
+                if (!int.TryParse(Console.ReadLine(), out int result))
                 {
                     Console.WriteLine("Число не считывается.");
                     continue;
                 }
-                ArrayAdd(arr, result);
-                
-                break;
+                ArrayAdd(ref arr, result);
+                goto case 3;
             case 2:
                 Console.WriteLine("Введите искомое число");
                 if (!int.TryParse(Console.ReadLine(), out result))
@@ -29,33 +36,39 @@ while (true)
                     Console.WriteLine("Число не считывается.");
                     continue;
                 }
-                ArrayRemove(arr, result);
-                break;
+                ArrayRemove(ref arr, result);
+                goto case 3;
             case 3:
                 ArrayPrint(arr);
                 break;
             default:
-                continue;
+                check = false;
+                break;
         }
     }
-
-
 }
-static int[] ArrayAdd(int[] arr, int num)
+
+static void ArrayAdd(ref int[] arr, int num)
 {
+    if (arr.Length == 0)
+    {
+        arr = new int[1] { num };
+        return;
+    }
+
     int[] resultArr = new int[arr.Length + 1];
     if (arr[0] >= num)
     {
         resultArr[0] = num;
-        for (int i = 1; i < resultArr.Length; i++)
+        for (int i = 1; i <= arr.Length; i++)
         {
             resultArr[i] = arr[i - 1];
         }
     }
-    else if (arr[arr.Length] <= num)
+    else if (arr[arr.Length - 1] <= num)
     {
-        resultArr[resultArr.Length] = num;
-        for (int i = 0; i < resultArr.Length; i++)
+        resultArr[resultArr.Length - 1] = num;
+        for (int i = 0; i < arr.Length; i++)
         {
             resultArr[i] = arr[i];
         }
@@ -63,32 +76,33 @@ static int[] ArrayAdd(int[] arr, int num)
     else
     {
         bool flag = true;
-        for (int i = 0; i < resultArr.Length-1; i++)
+        resultArr[0] = arr[0];
+        for (int i = 1; i < resultArr.Length; i++)
         {
             if (flag)
             {
-                if (num >= arr[i] && num <= arr[i + 1])
+                if (num >= arr[i - 1] && num <= arr[i])
                 {
                     flag = false;
-                    arr[i] = num;
+                    resultArr[i] = num;
                     continue;
                 }
                 resultArr[i] = arr[i];
             }
             else
             {
-                resultArr[i] = arr[i + 1];
+                resultArr[i] = arr[i - 1];
             }
         }
     }
-    return resultArr;
+    arr = resultArr;
 }
 
-int[] ArrayRemove (int[] arr, int num)
+static void ArrayRemove (ref int[] arr, int num)
 {
     if (num < arr[0] || num > arr[arr.Length-1])
     {
-        return arr;
+        return;
     }
     int[] resultArr = new int[arr.Length - 1];
     bool flag = true;
@@ -109,18 +123,20 @@ int[] ArrayRemove (int[] arr, int num)
         }
     }
 
-    return flag? arr:resultArr;
+    if(!flag) arr = resultArr;
 }
 
-void ArrayPrint (int[] arr)
+static void ArrayPrint (int[] arr)
 {
     if (arr.Length == 0)
     {
         Console.WriteLine("Пустой массив");
         return;
     }
+    Console.Write('[');
     foreach(int i in arr)
     {
-        Console.Write(i + " ");
+        Console.Write(i + ", ");
     }
+    Console.Write("\b\b]\n");
 }
